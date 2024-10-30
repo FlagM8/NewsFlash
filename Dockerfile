531934e091e5b1d452e8d02b2f5263fd9d1c37c9
Incredible-Seilaam <natalkarutova@seznam.cz>
2024-10-30 15:32:38 +0100

    funguje to yippieeeee

--- Dockerfile
+++ Dockerfile
@@ -12,4 +12,3 @@ EXPOSE 5001
 COPY . .
 
 CMD [ "python", "-m", "flask", "run", "--host=0.0.0.0", "--port=5001" ]
- 
--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 # app.py
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route("/home")
+def home_page():
+    return 'hello world hihi kith'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port="5001",debug=True)
