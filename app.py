d67a4f0ddd36765db333f81ff4fbf146c31cef5a
Incredible-Seilaam <natalkarutova@seznam.cz>
2024-10-31 11:12:15 +0100

    main page temp

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route("/home")
 def home_page():
-    return 'hello world hihi kith'
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port="5001",debug=True)
