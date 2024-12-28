fc3a5b05a815dadb280959558d2fe0a5190024ca
Jan Čmuchař <126028643+FlagM8@users.noreply.github.com>
2024-12-28 20:41:33 +0100

    login

---
+++ services/frontend/src/components/Register.vue
@@ -0,0 +1,37 @@
+<template>
+    <form @submit.prevent="register">
+      <input v-model="username" placeholder="Username" required />
+      <input v-model="email" type="email" placeholder="Email" required />
+      <input v-model="password" type="password" placeholder="Password" required />
+      <button type="submit">Register</button>
+    </form>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  export default {
+    data() {
+      return {
+        username: '',
+        email: '',
+        password: ''
+      };
+    },
+    methods: {
+      async register() {
+        try {
+          await axios.post('/api/register', {
+            username: this.username,
+            email: this.email,
+            password: this.password
+          });
+          this.$router.push('/login');
+        } catch (error) {
+          console.error(error.response.data.message);
+        }
+      }
+    }
+  };
+  </script>
+  
